cb6472ccce9c83c12ad5d54b1e6164c859d8457f
James Walmsley <james.walmsley@chargepoint.com>
2024-01-29 15:16:31 +0000

    ci: fixup teleport

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -48,10 +48,10 @@ Main() {
 
 init_chargepoint_rootfs() {
 	echo 'alias cp.h="env GIT_WORK_TREE=$HOME GIT_DIR=$HOME/.config/chargepoint/.dot-files.git' >> /etc/skel/.bashrc
-	echo 'alias cp.r="env GIT_WORK_TREE=/ GIT_DIR=$HOME/.config/chargepoint/.root.dot-files.git' >> /etc/skel/.bashrc
+	echo 'alias cp.r="env GIT_WORK_TREE=/ GIT_DIR=$HOME/.config/chargepoint/.root.dot-files.git sudo -E' >> /etc/skel/.bashrc
 	cp /tmp/overlay/cp-setup /usr/bin
 	apt-get -y install curl git neovim fish
-	curl https://goteleport.com/static/install.sh | bash -s 14.3.3
+	curl https://goteleport.com/static/install.sh | bash -s 13.4.15
     curl -d "accept_license_agreement=accepted&submit=Download+software" -X POST -O "https://www.segger.com/downloads/jlink/JLink_Linux_arm64.deb"
 	apt-get -y install ./JLink_Linux_arm64.deb
 	rm ./JLink_Linux_arm64.deb
