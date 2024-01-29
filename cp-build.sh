35fc8e80bc2f275e158de5184785b42f9e2c8786
James Walmsley <james.walmsley@chargepoint.com>
2024-01-29 14:38:24 +0000

    ci: update workflow to support xz images and cp-setup script

--- cp-build.sh
+++ cp-build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #
-./compile.sh BRANCH=current RELEASE=jammy BUILD_MINIMAL=yes KERNEL_CONFIGURE=no BOARD=rockpi-4cplus VENDOR=ChargePoint-Armbian
-
+./compile.sh BRANCH=current RELEASE=jammy BUILD_MINIMAL=yes KERNEL_CONFIGURE=no BOARD=rockpi-4cplus VENDOR=ChargePoint-Armbian COMPRESS_OUTPUTIMAGE=sha,xz IMAGE_XZ_COMPRESSION_RATION=5
 
