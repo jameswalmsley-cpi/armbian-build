84e34c6664853d4bef989728c53c8a3ba8ad2fe0
James Walmsley <james.walmsley@chargepoint.com>
2024-01-30 13:23:29 +0000

    cp: add pyvisa packages required to work with usb test equipment

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -45,17 +45,17 @@ Main() {
 	esac
 } # Main
 
-
 init_chargepoint_rootfs() {
 	echo 'alias cp.h="env GIT_WORK_TREE=$HOME GIT_DIR=$HOME/.config/chargepoint/.dot-files.git"' >> /etc/skel/.bashrc
 	echo 'alias cp.r="env GIT_WORK_TREE=/ GIT_DIR=$HOME/.config/chargepoint/.root.dot-files.git sudo -E"' >> /etc/skel/.bashrc
 	cp /tmp/overlay/cp-setup /usr/bin
 	apt-get update
-	apt-get -y install curl git neovim fish tmux
+	apt-get -y install curl git neovim fish tmux python3-pip python3-usb python3-pyvisa python3-pyvisa-py
 	curl https://goteleport.com/static/install.sh | bash -s 13.4.15
     curl -d "accept_license_agreement=accepted&submit=Download+software" -X POST -O "https://www.segger.com/downloads/jlink/JLink_Linux_arm64.deb"
 	apt-get -y install ./JLink_Linux_arm64.deb
 	rm ./JLink_Linux_arm64.deb
+	curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
 }
 
 Main "$@"
